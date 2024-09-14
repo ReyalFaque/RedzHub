8b3e4f2601cd6372e37ea003071b18cf79412ca7
ReyalFaque <esterlinacorro@gmail.com>
2024-09-14 17:30:45 +0800

    Create redzhub.lua

---
+++ redzhub.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/MemeSea/main/Source.lua"))()
+
+wait(0.2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ReyalFaque/RedzHub/main/loader.lua"))()
