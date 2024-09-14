fd2db8c0362b2bf9619c4b4756a3821fa8f34839
ReyalFaque <esterlinacorro@gmail.com>
2024-09-14 21:13:54 +0800

    Update redzhub.lua

--- redzhub.lua
+++ redzhub.lua
@@ -1,5 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/MemeSea/main/Source.lua"))()
 
-wait(0.2)
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ReyalFaque/RedzHub/main/loader.lua"))()
